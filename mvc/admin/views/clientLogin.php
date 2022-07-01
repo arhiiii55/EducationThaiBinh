491d849e30df34169884b3b4da8d65fcaa9f5f2e
Duc Tai99 <ductaiit99@gmail.com>
2022-07-01 13:21:17 +0700

    add client login page

---
+++ mvc/admin/views/clientLogin.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Free Stylish Login Page Website Template | Smarteyeapps.com</title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/clientLogin.css" />
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="no-pdding login-box">
+            <div class="row no-margin w-100 bklmj">
+                <div class="col-lg-6 col-md-6 log-det" style="
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+            ">
+                    <h2 style="margin-bottom: 40px">Đăng nhập</h2>
+                    <div class="text-box-cont">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Tên tài khoản" aria-label="Username"
+                                aria-describedby="basic-addon1" />
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Mật khẩu" aria-label="Username"
+                                aria-describedby="basic-addon1" />
+                        </div>
+                        <br />
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 box-de">
+                    <div class="ditk-inf">
+                        <h2 class="w-100">Thai Binh Spots</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+</html>
